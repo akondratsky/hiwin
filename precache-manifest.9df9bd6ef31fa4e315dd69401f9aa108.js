e1a31c803a791db31d28ccfafd2966ea6f81a20f
Aleksandr <akondratsky@yandex.ru>
2020-02-29 04:38:14 +0400

    init pages in master

---
+++ precache-manifest.9df9bd6ef31fa4e315dd69401f9aa108.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bcd88fa3b906a10e6710024a353ae54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec1e6f16dc75f7f629fc",
+    "url": "/static/css/main.d5cefc6f.chunk.css"
+  },
+  {
+    "revision": "38c7579245b74b010e7d",
+    "url": "/static/js/2.7f4791ad.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.7f4791ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec1e6f16dc75f7f629fc",
+    "url": "/static/js/main.e086bb99.chunk.js"
+  },
+  {
+    "revision": "de69a620ac7f1de5e85f",
+    "url": "/static/js/runtime-main.d5781508.js"
+  }
+]);
